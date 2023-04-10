eb81fd58e4013a146434f3f1a014630ca2a39b6c
vinaykanth97 <vinaykennedy@gmail.com>
2023-04-11 00:11:03 +0530

    dependency setup and sass structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import { AddBooks } from './components/AddBooks';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import BookList from './components/BookList';
-
 function App() {
   return (
     <div className="App container">
-      <h1 className='display-3 text-center mb-5'>Book Store</h1>
-      <AddBooks />
-      <BookList />
+      <p>lorem</p>
     </div>
   );
 }
--- src/reducer/bookRecordReducer.js
+++ src/reducer/bookRecordReducer.js
@@ -1,35 +1,11 @@
 const bookRecords = {
-    books: [],
-    bookValues: [],
-    updateBookValues: []
+    dashboard: []
 }
 
 export const bookRecordReducer = (state = bookRecords, action) => {
-    // console.log(state)
     switch (action.type) {
-        case "BOOKRECORDS":
-            return {
-                ...state, books: [...state.books, { ...action.payload.books, id: action.payload.id }]
-            }
-
-        case "BOOKDETAIL":
-            return { ...state, bookValues: action.payload.bookValues }
-
-        case "DELETEBOOKRECORD":
-            return { ...state, books: state.books.filter(book => book.id !== action.payload.id) }
-
-        case "UPDATEBOOKVALUES":
-            return { ...state, updateBookValues: action.payload.updateBookValues }
-
-        case "UPDATEBOOKRECORD":
-            return {
-                ...state, books: state.books.filter(book => {
-                    if (book.id === action.payload.id) {
-                        book.bookTitle = action.payload.bookTitle
-                    }
-                    return book
-                })
-            }
+        case "DASHBOARD":
+            return { ...state }
 
         default:
             return { ...state }
