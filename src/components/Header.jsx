838c07cf1ba87f8a4022f51a4ffd7a0c4dceef94
vinaykanth97 <vinaykennedy@gmail.com>
2023-04-15 00:51:29 +0530

    em changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,28 +9,33 @@ import CustomisablePopup from './CustomisablePopup'
 export default function Header() {
     return (
         <header>
-            <a href="javascript:void(0)" className='logo'>
-                <img src={Logo} alt="logo" />
-            </a>
-            <form className="search">
-                <input type="search" placeholder='Search' />
-                <button><img src={searchIcon} alt="search" /></button>
-            </form>
-            <div className="utilities">
-                <div className="utilities-help utilities-option">
-                    <img src={helpIcon} alt="help" />
-                    <CustomisablePopup popupId="help" />
+            <div className="container">
+                <div className="d-flex header-wrap">
+                    <div className='logo'>
+                        <img src={Logo} alt="logo" />
+                    </div>
+                    <form className="search">
+                        <input type="search" placeholder='Search' />
+                        <button><img src={searchIcon} alt="search" /></button>
+                    </form>
+                    <div className="utilities">
+                        <div className="utilities-help utilities-option">
+                            <img src={helpIcon} alt="help" />
+                        </div>
+                        <div className="utilities-notification utilities-option">
+                            <img src={notificationIcon} alt="notification" />
+                        </div>
+                        <div className="utilities-calendar utilities-option">
+                            <img src={calendarIcon} alt="Calendar" />
+                        </div>
+                        <div className="utilities-user utilities-option">
+                            <img src={userIcon} alt="user" />
+                        </div>
+                    </div>
                 </div>
-                <div className="utilities-notification utilities-option">
-                    <img src={notificationIcon} alt="notification" />
-                </div>
-                <div className="utilities-calendar utilities-option">
-                    <img src={calendarIcon} alt="Calendar" />
-                </div>
-                <div className="utilities-user utilities-option">
-                    <img src={userIcon} alt="user" />
-                </div>
+
             </div>
+
         </header>
     )
 }
